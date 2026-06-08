f0252989366f5f8bd9e03eb76d8bea330f126960
EXLS-1 <excellentservice1exls@gmail.com>
2026-06-08 06:40:36 +0100

    refactor: restructure PrismaClient implementation and add seed file for initial data population

--- lib/auth/auth.ts
+++ lib/auth/auth.ts
@@ -2,7 +2,7 @@
 
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
-import prisma from "@/lib/prisma/prisma";
+import prisma from "@/lib/prisma";
 
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
